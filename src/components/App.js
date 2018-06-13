6169a6a9f43c3fae6e3d8fca6b512ee570c30730
Rick Clark <rkingsleyclark@gmail.com>
2018-06-13 08:22:48 +0100

    Add initial image data

---
+++ src/components/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import images from '../data/images.json';
+import './App.css';
+
+export default function App() {
+  console.log(images);
+  return <div>hello world</div>;
+}
