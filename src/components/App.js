4619277e51d7d52bb5fcdba450571db09daa0577
Rick Clark <rkingsleyclark@gmail.com>
2018-06-13 08:55:11 +0100

    Rotate through images

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import images from '../config/images.json';
+import { SLIDESHOW_SPEED } from '../config/appSettings';
 import './App.css';
 
 export default class App extends React.Component {
@@ -10,10 +11,28 @@ export default class App extends React.Component {
     };
   }
 
+  componentDidMount() {
+    window.setInterval(() => {
+      const newImageIndex =
+        this.state.imageIndex === images.length - 1
+          ? 0
+          : this.state.imageIndex + 1;
+
+      this.setState({
+        imageIndex: newImageIndex,
+      });
+    }, SLIDESHOW_SPEED);
+  }
+
   render() {
+    const image = images[this.state.imageIndex];
+
     return (
-      <div>
-        <img src={images[this.state.imageIndex].src} alt="Curent slide" />
+      <div className="container">
+        <img className="image" src={image.src} alt="Curent slide" />
+        <span className={`watermark watermark-${image.watermarkPosition}`}>
+          {image.author}
+        </span>
       </div>
     );
   }
